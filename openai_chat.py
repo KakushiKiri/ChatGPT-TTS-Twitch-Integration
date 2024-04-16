0aaca1865efb834916836cebb51119b3837b42d6
Spencer Hix <spencerhix@gmail.com>
2024-04-16 12:48:51 -0500

    basic debugging: fixed token_count not being updated on behavior set, and removed code that is not used

--- openai_chat.py
+++ openai_chat.py
@@ -1,9 +1,8 @@
 import os
-import asyncio
 import json
-import time
 import tiktoken
 from openai import OpenAI, AuthenticationError
+
 class AI:
     #class initializer; contains a list and OpenAI instance
     def __init__(self):
@@ -15,6 +14,7 @@ class AI:
         except AuthenticationError:
             exit('ERROR -- You Dummy, you have the wrong API Key')
 
+    #check_tokens; Checks the tokens of an input, raises an error if it exceeds limit
     def check_tokens(self, input):
         encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
         if len(encoding.encode(input)) <= 4096:
@@ -23,31 +23,29 @@ class AI:
             raise ValueError('ERROR -- Given Prompt Exceeds Token Limit')
 
     #handle_input; takes prompt as parameter
-    def handle_input(self, prompt):
+    def handle_input(self, prompt, user='Kiri'):
+        #check tokens of input, if it passes append to both history variables
         self.check_tokens(prompt)
-        
-        curr = time.time()
-        s = time.strftime('%a %d %b %Y %H:%M:%S', time.localtime(curr))
-        self.conv_history.append({'role': 'user', 'content': prompt})
-        self.full_history.append({'time': s, 'role': 'user', 'content': prompt})
+        self.conv_history.append({'role': user, 'content': prompt})
+        self.full_history.append({'role': user, 'content': prompt})
 
+        #check current token count and conv_history length; begin deleting list elements if either checks pass
         if self.token_count > 13000 or len(self.conv_history) > 20:
-            while len(self.conv_history) > 10:
+            while len(self.conv_history) > 20 or self.token_count > 13000:
                 encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
                 self.token_count -= len(encoding.encode(self.conv_history[1]['content']))
                 del self.conv_history[1]
 
-        prepare = self.client.chat.completions.create(  #send question to ChatGPT by sending entire conversation history
+        #send API request to OpenAI; you may change model if you have access but other parts of code might need changing
+        prepare = self.client.chat.completions.create(
             model='gpt-3.5-turbo',
             messages=self.conv_history
         )
 
-        curr = time.time()
-        s = time.strftime('%a %d %b %Y %H:%M:%S', time.localtime(curr))
-        #similar to the question, retrieve it and append to the conversation history, then return response
+        #similar to the question, retrieve the response and append to the conversation history, then return response
         response =  prepare.choices[0].message.content
         self.conv_history.append({'role': 'assistant', 'content': response})
-        self.full_history.append({'time': s, 'role': 'assistant', 'content': response})
+        self.full_history.append({'role': 'assistant', 'content': response})
 
         return response
     
@@ -57,32 +55,35 @@ class AI:
         if os.path.isfile('behavior.txt'):
             with open('behavior.txt', 'r') as fname:    #open file and store contents in variable; then call handle_input with said variable
                 behavior = fname.read().strip()
+            self.check_tokens(behavior)
             self.conv_history.append({'role': 'user', 'content': behavior})
         else:
             raise FileNotFoundError('ERROR -- No Behavior File Found')
     
+    #dump_history; if program breaks, the conversation history will be dumped into a file
     def dump_history(self):
         with open('backup_history.json', 'w+') as fname:
             json.dump(self.full_history, fname)
 
-#test code
-def main():
-    ai_test = AI()
-    ai_test.set_behavior()
-    print("Welcome to the ChatGPT API test!!\nInput whatever questions you have and/or type 'exit' to end the chat!\n")
-    while True:
-        question = input('What Question do you Have?: ')
-        if question == 'exit' or question == 'Exit':
-            break
-        elif question == 'tokens' or question == 'Tokens':
-            print(f'\n{ai_test.token_count}\n')
-        elif question == 'history' or question == 'History':
-            for i in ai_test.conv_history:
-                print(f'{i['role']}: {i['content']}')
-        elif question == 'dump' or question == 'Dump':
-            ai_test.dump_history()
-            break
-        else:
-            print(f'\n{ai_test.handle_input(question)}\n')
 
-main()
+# Use this to test only this file
+
+# def main():
+#     ai_test = AI()
+#     ai_test.set_behavior()
+#     print("Welcome to the ChatGPT API test!!\nInput whatever questions you have and/or type 'exit' to end the chat!\n")
+#     while True:
+#         question = input('What Question do you Have?: ')
+#         if question == 'exit' or question == 'Exit':
+#             break
+#         elif question == 'tokens' or question == 'Tokens':
+#             print(f'\n{ai_test.token_count}\n')
+#         elif question == 'history' or question == 'History':
+#             for i in ai_test.conv_history:
+#                 print(f'{i['role']}: {i['content']}')
+#         elif question == 'dump' or question == 'Dump':
+#             ai_test.dump_history()
+#             break
+#         else:
+#             print(f'\n{ai_test.handle_input(question)}\n')
+# main()
