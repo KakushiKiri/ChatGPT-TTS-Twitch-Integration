4c6c2ba980a55099378da3c51ebd8b67896e84ef
Spencer Hix <spencerhix@gmail.com>
2024-04-25 23:38:30 -0500

    began optimization work on tts

--- elevenlabs_tts.py
+++ elevenlabs_tts.py
@@ -1,7 +1,10 @@
-from os import getenv, getcwd
+from os import getenv, getcwd, remove
+from asyncio import wait
 from os.path import join
 from elevenlabs.client import ElevenLabs
 from elevenlabs import save
+from playsound import playsound
+from mutagen.mp3 import MP3
 
 class TTS_Manager:
     def __init__(self):
@@ -9,7 +12,11 @@ class TTS_Manager:
         self.voices = self.client.voices.get_all()
         self.main_voice = 'Dave'
 
-    def file_tts(self, input, file_name='current_line.wav'):
+    def file_length(self, file):
+        audio = MP3(file)
+        return audio.info.length
+
+    def file_tts(self, input, file_name='current_line.mp3'):
         audio = self.client.generate(
             text = input,
             voice = self.main_voice,
@@ -17,6 +24,11 @@ class TTS_Manager:
         )
         dir = join(getcwd(), file_name)
         save(audio, dir)
+        length = self.file_length(file_name)
+        playsound(dir, False)
+        wait(int(length) + 2)
+        remove(dir)
+
 if __name__ == '__main__':
     tts = TTS_Manager()
     tts.file_tts('Hello, my name is Dave.')
