f0aac837fbdd98c29b16774cd50daff9e86f4989
Spencer Hix <spencerhix@gmail.com>
2024-04-29 21:48:53 -0500

    continued work on optimization

--- elevenlabs_tts.py
+++ elevenlabs_tts.py
@@ -1,14 +1,18 @@
 from os import getenv, getcwd, remove
-from asyncio import wait
+from time import sleep
+from asyncio import wait, run
 from os.path import join
-from elevenlabs.client import ElevenLabs
-from elevenlabs import save
+from elevenlabs.client import ElevenLabs, AsyncElevenLabs
+from elevenlabs import save, stream, play
 from playsound import playsound
 from mutagen.mp3 import MP3
 
 class TTS_Manager:
-    def __init__(self):
-        self.client = ElevenLabs(api_key=getenv('ELEVENLABS_API_KEY'))
+    def __init__(self, sync=True):
+        if sync:
+            self.client = ElevenLabs(api_key=getenv('ELEVENLABS_API_KEY'))
+        else:
+            self.client = AsyncElevenLabs(api_key=getenv('ELEVENLABS_API_KEY'))
         self.voices = self.client.voices.get_all()
         self.main_voice = 'Dave'
 
@@ -16,7 +20,12 @@ class TTS_Manager:
         audio = MP3(file)
         return audio.info.length
 
-    def file_tts(self, input, file_name='current_line.mp3'):
+    def play_file(self, file):
+        dir = join(getcwd(), file)
+        playsound(dir)
+        remove(dir)
+
+    async def file_tts(self, input, file_name='current_line.mp3'):
         audio = self.client.generate(
             text = input,
             voice = self.main_voice,
@@ -24,11 +33,19 @@ class TTS_Manager:
         )
         dir = join(getcwd(), file_name)
         save(audio, dir)
-        length = self.file_length(file_name)
-        playsound(dir, False)
-        wait(int(length) + 2)
-        remove(dir)
-
+    
+    def stream_tts(self, input):
+        audio_stream = self.client.generate(
+            text=input,
+            stream=True
+        )
+        stream(audio_stream)
+        
 if __name__ == '__main__':
-    tts = TTS_Manager()
-    tts.file_tts('Hello, my name is Dave.')
+    tts = TTS_Manager(True)
+
+    tts.stream_tts("How is it going, my name is john dark souls.")
+
+    # run(tts.file_tts("Hello, my name is Dave"))
+    # dir = join(getcwd(), 'current_line.mp3')
+    # tts.play_file(dir)
