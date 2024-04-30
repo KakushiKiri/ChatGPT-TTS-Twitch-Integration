90533b6a27c1d98ad7574da054824926e1e67ee7
Spencer Hix <spencerhix@gmail.com>
2024-04-29 21:50:05 -0500

    changed script based on other file changes

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from speech_to_text_async import Azure_Manager
+from speech_to_text import Azure_Manager
 from openai_chat import AI_Manager
 from elevenlabs_tts import TTS_Manager
 import asyncio
@@ -12,12 +12,12 @@ ELEVENLABS = TTS_Manager()
 while True:
     if keyboard.is_pressed('/'):
         print('Listening to Mic -- Press Q to End')
-        message = asyncio.run(AZURE.test_message())
+        message = asyncio.run(AZURE.continuous_mic_input(sync=True))
         print(f'Here is Your Message: {message}\n--- End Mic Input ---')
-        result = AI.handle_input(message)
+        result = AI.handle_input(message, False)
         print(f'AI Response: {result}')
-        ELEVENLABS.file_tts(result)
-
+        asyncio.run(ELEVENLABS.file_tts(result))
+        ELEVENLABS.play_file('current_line.mp3')
 
     time.sleep(0.1)
 
