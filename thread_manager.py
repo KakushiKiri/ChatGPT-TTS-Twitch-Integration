cea4e94e933c12c23a575130c78b8d6572baa492
Spencer Hix <spencerhix@gmail.com>
2024-05-29 00:14:48 -0500

    added documentation and simplified small parts until further work

--- thread_manager.py
+++ thread_manager.py
@@ -1,5 +1,5 @@
 from openai_chat import AI_Manager
-from elevenlabs_tts import TTS_Manager
+# from elevenlabs_tts import TTS_Manager
 from speech_to_text import Azure_Manager
 from vad_manager import VAD_Manager
 from threading import Thread, Event
@@ -11,37 +11,51 @@ import torch
 
 class Thread_Manager():
     def __init__(self):
+        """initialized Thread_Manager to encompass the main logic of the program
+        
+           no params
+
+           init: speech_queue, tts_queue -- queue's shared between Thread_Manager and Azure_Manager/AI_Manager respectively
+                 found_speech -- Thread event for the VAD to signal the start of the Speech-to-Text
+                 pause_vad -- Thread event for the VAD to pause after Speech-to-Text is finished
+
+                 AI, STT, VAD -- Three main classes imported from their respective files
+
+                 vad/ai/recognition/tts_thread -- variables that threads will be assigned to
+        """
         self.speech_queue = queue.Queue(5)
         self.tts_queue = queue.Queue(5)
 
         self.found_speech = Event()
         self.pause_vad = Event()
-        self.finished = False
-        self.continue_recording = True
-
+        
         self.AI = AI_Manager(self.tts_queue)
-        # self.TTS = TTS_Manager()
         self.STT = Azure_Manager(self.speech_queue)
         self.VAD = VAD_Manager()
 
-        self.vad_thread = None
-        self.ai_thread = None
-        self.recognition_thread = None
-        self.tts_thread = None
+        self.vad_thread = Thread(target=self.VAD_Thread)
+        self.ai_thread = Thread(target=self.AI_Thread)
+        self.recognition_thread = Thread(target=self.Recognition_Thread)
+        self.tts_thread = Thread(target=self.TTS_Thread)
+
+        self.thread_list = [self.vad_thread, self.recognition_thread, self.ai_thread, self.tts_thread]
 
     def VAD_Thread(self):
+        """same logic as vad_manager.py
+        
+           no params
+        """
         stream = self.VAD.audio.open(format=self.VAD.FORMAT,
                         channels=self.VAD.CHANNELS,
                         rate=self.VAD.SAMPLE_RATE,
                         input=True,
                         frames_per_buffer=self.VAD.CHUNK)
 
-        while self.continue_recording:
+        while True:
             audio_chunk = stream.read(self.VAD.num_samples)
             audio_int16 = np.frombuffer(audio_chunk, np.int16)
             audio_float32 = self.VAD.int2float(audio_int16)
 
-            # get the confidences
             new_confidence = self.VAD.model(torch.from_numpy(audio_float32), 16000).item()
             #print(new_confidence)
             if new_confidence >= 0.6:
@@ -50,64 +64,62 @@ class Thread_Manager():
                 self.pause_vad.wait()
                 self.pause_vad.clear()
                 stream.start_stream()
-        stream.stop_stream()
-        self.VAD.audio.terminate()
-        print('Stopped Recording')
     
     def Recognition_Thread(self):
-        while not self.finished:
+        """waits for event from VAD_Thread, then runs continuous mic input
+           clears found_speech event to loop and wait again
+
+           no params
+        """
+        while True:
             self.found_speech.wait()
-            self.STT.continuous_mic_input(sync=True)
+            self.STT.continuous_mic_input()
             self.found_speech.clear()
 
     def AI_Thread(self):
-        while not self.finished:
+        """does not rely on an event, but will be in a sleeping state until an item is in the speech_queue
+           it then sends it to the AI to process
+        
+           no params
+        """
+        while True:
             if not self.speech_queue.empty():
                 self.AI.handle_input(self.speech_queue.get())
             time.sleep(0.1)
 
     def TTS_Thread(self):
-        while not self.finished:
+        """similar to AI_Thread, sleeps until item is in tts_queue, plays audio, and then resumes VAD
+
+           no params
+        """
+        while True:
             if not self.tts_queue.empty():
                 self.STT.azure_tts(self.tts_queue.get())
                 self.pause_vad.set()
-                # def text_stream():
-                #     for i in self.tts_list:
-                #         yield i
-                
-                # audio_stream = self.TTS.client.generate(
-                #     text=text_stream(),
-                #     voice=self.TTS.main_voice,
-                #     model = 'eleven_multilingual_v2',
-                #     stream=True
-                # )
-                # self.pause_vad=False
-                # self.TTS.stream_tts(audio_stream=audio_stream)
             time.sleep(0.1)
 
     def Create_Threads(self):
-        self.vad_thread = Thread(target=self.VAD_Thread)
-        self.recognition_thread = Thread(target=self.Recognition_Thread)
-        self.ai_thread = Thread(target=self.AI_Thread)
-        self.tts_thread = Thread(target=self.TTS_Thread)
-
-        self.vad_thread.start()
-        self.recognition_thread.start()
-        self.ai_thread.start()
-        self.tts_thread.start()
+        """loops through thread_list and starts each one
+        
+           no params
+        """
+        for thread in self.thread_list:
+            thread.start()
     
     def stop_threads(self):
-        self.vad_thread.join(timeout=0.5)
-        self.ai_thread.join(timeout=0.5)
-        self.recognition_thread.join(timeout=0.5)
-        self.tts_thread.join(timeout=0.5)
+        """loops through thread_list and ends each one
+        
+           no params
+        """
+        for thread in self.thread_list:
+            thread.join(timeout=0.5)
         self.VAD.audio.terminate()
 
 if __name__ == '__main__':
     Threads = Thread_Manager()
-    # if not Threads.finished:
-    Threads.finished = False
     Threads.Create_Threads()
-    while not Threads.finished:
+    running = True
+    while running:
         if keyboard.is_pressed('p'):
             Threads.stop_threads()
+            running = False
